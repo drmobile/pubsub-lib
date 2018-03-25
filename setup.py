d72ac6fcbc33f5c2e23836ad2df9cc9b05b2a3eb
Jonas <jonas@Jonass-MacBook-Air.local>
2018-03-25 11:19:30 +0800

    make this library as pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubsub-broker',
+    version='0.1',
+    url='https://github.com/drmobile/pubsub-broker',
+    license='Apache Software License',
+    author='Soocii',
+    author_email='service@soocii.me',
+    description='Library for Soocii back-end services to integrate with Google Cloud Pub/Sub service.',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    zip_safe=False)
+    
