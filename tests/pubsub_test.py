f1e658833beb8cb1d4c788f88956d6f74fca602c
Jonas <jonas@Jonass-MacBook-Air.local>
2018-03-28 22:05:44 +0800

    mark out long running test cases, need to figure out better way

--- tests/pubsub_test.py
+++ tests/pubsub_test.py
@@ -113,54 +113,54 @@ class NormalPublishTests(unittest.TestCase):
         # verify if message_id is returned
         self.assertTrue(message_id is not None)
 
-# abnormal publish
-@pytest.mark.usefixtures("start_emulator")
-class AbnormalPublishTests(unittest.TestCase):
-    def setUp(self):
-        self.project = 'fake-project'
-        self.cred = None
-        self.topic = 'fake-topic'
-        self.published_message_id = None
+# # abnormal publish
+# @pytest.mark.usefixtures("start_emulator")
+# class AbnormalPublishTests(unittest.TestCase):
+#     def setUp(self):
+#         self.project = 'fake-project'
+#         self.cred = None
+#         self.topic = 'fake-topic'
+#         self.published_message_id = None
 
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
-    def __on_published(self, message_id):
-        logger.info('message is published with message id: {}'.format(message_id))
-        self.published_message_id = message_id
+#     def __on_published(self, message_id):
+#         logger.info('message is published with message id: {}'.format(message_id))
+#         self.published_message_id = message_id
 
-    def test_publish_to_non_exist_topic(self):
-        # prepare publisher
-        publisher = pubsub_client.PublisherClient(self.project, self.cred)
+#     def test_publish_to_non_exist_topic(self):
+#         # prepare publisher
+#         publisher = pubsub_client.PublisherClient(self.project, self.cred)
 
-        publisher.publish('non-exist', b'bytes data', callback=lambda message_id: self.__on_published(message_id))
-        # wait for callback
-        time.sleep(1)
-        # verify if message has NOT been published
-        self.assertTrue(self.published_message_id is None)
+#         publisher.publish('non-exist', b'bytes data', callback=lambda message_id: self.__on_published(message_id))
+#         # wait for callback
+#         time.sleep(1)
+#         # verify if message has NOT been published
+#         self.assertTrue(self.published_message_id is None)
 
-@pytest.mark.usefixtures("no_emulator")
-class NoEmulatorPublishTests(unittest.TestCase):
-    def setUp(self):
-        self.project = 'fake-project'
-        self.cred = None
-        self.topic = 'fake-topic'
-        self.published_message_id = None
+# @pytest.mark.usefixtures("no_emulator")
+# class NoEmulatorPublishTests(unittest.TestCase):
+#     def setUp(self):
+#         self.project = 'fake-project'
+#         self.cred = None
+#         self.topic = 'fake-topic'
+#         self.published_message_id = None
 
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
-    def __on_published(self, message_id):
-        logger.info('message is published with message id: {}'.format(message_id))
-        self.published_message_id = message_id
+#     def __on_published(self, message_id):
+#         logger.info('message is published with message id: {}'.format(message_id))
+#         self.published_message_id = message_id
 
-    def test_publish_without_emulator(self):
-        # prepare publisher
-        publisher = pubsub_client.PublisherClient(self.project, self.cred)
+#     def test_publish_without_emulator(self):
+#         # prepare publisher
+#         publisher = pubsub_client.PublisherClient(self.project, self.cred)
 
-        publisher.publish(self.topic, b'bytes data', callback=lambda message_id: self.__on_published(message_id))
-        # wait for callback
-        time.sleep(1)
-        # verify if message has NOT been published
-        self.assertTrue(self.published_message_id is None)
+#         publisher.publish(self.topic, b'bytes data', callback=lambda message_id: self.__on_published(message_id))
+#         # wait for callback
+#         time.sleep(1)
+#         # verify if message has NOT been published
+#         self.assertTrue(self.published_message_id is None)
 
