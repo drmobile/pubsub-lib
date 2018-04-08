d4837e90c43e8b8d4fef543153cdbcf40fdee23b
Jonas Cheng <jonas.cheng@gmail.com>
2018-04-08 13:00:41 +0800

    added docstring

--- soocii_pubsub_lib/pubsub_client.py
+++ soocii_pubsub_lib/pubsub_client.py
@@ -30,24 +30,57 @@ class PubSubBase():
 
 class PublisherClient(PubSubBase):
     def __init__(self, project, cred_json):
+        """A wrapped publisher client for Google Cloud Pub/Sub.
+
+        This creates an object that is capable of publishing messages. Generally, you can instantiate this client with no arguments, and you get sensible defaults.
+
+        Arguments:
+            project {str} -- Project id
+            cred_json {str} -- Full path to credential file in json format
+        """
         super(PublisherClient, self).__init__(project, cred_json)
         # Instantiates a client
         self.client = pubsub_v1.PublisherClient(credentials=self.cred)
 
-    # Creates the given topic with the given name.
     def create_topic(self, topic, **kwargs):
+        """Creates the given topic with the given name.
+
+        Arguments:
+            topic {str} -- The topic name to create.
+
+        Keyword Arguments:
+            retry {google.api_core.retry.Retry]} -- An optional retry object used to retry requests. If None is specified, requests will not be retried.
+
+        Returns:
+            str -- A topic name
+        """
         topic = self.topic_path(self.client, topic)
         try:
-            return self.client.create_topic(topic, **kwargs)
+            return self.client.create_topic(topic, **kwargs).name
         except AlreadyExists:
             logger.debug('topic {} already exists.'.format(topic))
 
-    # Gets the configuration of a topic.
     def get_topic(self, topic, **kwargs):
+        """Gets the configuration of a topic.
+
+        Arguments:
+            topic {str} -- The topic name to get.
+
+        Keyword Arguments:
+            retry {google.api_core.retry.Retry]} -- An optional retry object used to retry requests. If None is specified, requests will not be retried.
+
+        Raises:
+            google.api_core.exceptions.GoogleAPICallError -– If the request failed for any reason.
+            google.api_core.exceptions.RetryError –- If the request failed due to a retryable error and retry attempts failed.
+            ValueError -– If the parameters are invalid.
+
+        Returns:
+            str -- A topic name
+        """
         topic = self.topic_path(self.client, topic)
         return self.client.get_topic(topic, **kwargs).name
 
-    def on_published(self, future, callback):
+    def __on_published(self, future, callback):
         logger.debug('callback for publish message')
         message_id = None
         if future.cancelled():
@@ -63,13 +96,29 @@ class PublisherClient(PubSubBase):
         if callback is not None:
             callback(message_id)
 
-    # To publish a message, use the publish() method. This method accepts two positional arguments:
-    # the topic to publish to, and the payload of the message.
-    # It also accepts arbitrary keyword arguments, which are passed along as attributes of the message.
-    # If callback is provided, this method invoke in async way and return message_id through callback.
-    # If callback is NOT provided, this method invoke in sync way and return message_id.
-    # Either async or sync way invokation, this method return tuple (message_id, future)
+    #
     def publish(self, topic, payload, callback=None, **kwargs):
+        """To publish a message, use the publish() method. This method accepts two positional arguments:
+        the topic to publish to, and the payload of the message.
+        It also accepts arbitrary keyword arguments, which are passed along as attributes of the message.
+        If callback is provided, this method invoke in async way and return message_id through callback.
+        If callback is NOT provided, this method invoke in sync way and return message_id.
+        Either async or sync way invokation, this method return tuple (message_id, future)
+
+        Arguments:
+            topic {str} -- The topic name to publish messages to.
+            payload -- A bytestring, string, or dictionary representing the message body.
+
+        Keyword Arguments:
+            callback -- An optional callback (default: {None})
+
+        Raises:
+            ValueError -- If payload is not a bytestring, string, or dictionary.
+            Exception -- If unexpected exception is caughted
+
+        Returns:
+            (str, concurrent.futures.Future) -- A tuple of message id and future instance
+        """
         try:
             logger.debug('publish message to {}'.format(topic))
             dtype = type(payload)
@@ -87,7 +136,7 @@ class PublisherClient(PubSubBase):
             future = self.client.publish(topic, msg, **kwargs)
             # async call
             if callback is not None:
-                future.add_done_callback(lambda future: self.on_published(future, callback))
+                future.add_done_callback(lambda future: self.__on_published(future, callback))
                 return (None, future)
             # sync call
             else:
@@ -95,7 +144,7 @@ class PublisherClient(PubSubBase):
                 logger.info('data has been publised with message id {}.'.format(message_id))
                 return (message_id, future)
         except Exception as e:
-            logger.error('unexpected exception is caught: {}'.format(e))
+            logger.error('unexpected exception is caughted: {}'.format(e))
             raise e
 
 
