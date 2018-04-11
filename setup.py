cad5ec920ea448f3d3c92ee4e1ca19143c06a5b3
Jonas Cheng <jonas.cheng@gmail.com>
2018-04-11 11:02:33 +0800

    ver 1.2

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='soocii-pubsub-lib',
-    version='1.1',
+    version='1.2',
     url='https://github.com/drmobile/pubsub-broker',
     license='Apache Software License',
     author='Soocii',
     author_email='service@soocii.me',
     description='Library for Soocii back-end services to integrate with Google Cloud Pub/Sub service.',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests', 'samples']),
     long_description=open('README.md').read(),
     zip_safe=False,
 
