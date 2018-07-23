f93df5ec2367652ee10dae34d20094978d3e7f05
Jonas Cheng <jonas.cheng@gmail.com>
2018-07-23 11:26:00 +0800

    v1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='soocii-pubsub-lib',
-    version='1.5',
+    version='1.6',
     url='https://github.com/drmobile/pubsub-broker',
     license='Apache Software License',
     author='Soocii',
